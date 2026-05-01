d3a39eb0d273ab80e1be284f2090929072095e1f
Sharad Bista <bishalbista737@gmail.com>
2026-05-01 23:03:37 +0545

    feat(backend): implement index.js logic

---
+++ backend/controllers/index.js
@@ -0,0 +1,15 @@
+const authController = require('./authController');
+const bookController = require('./bookController');
+const reviewController = require('./reviewController');
+const adminController = require('./adminController');
+const dashboardController = require('./dashboardController');
+const recommendationController = require('./recommendationController');
+
+module.exports = {
+  authController,
+  bookController,
+  reviewController,
+  adminController,
+  dashboardController,
+  recommendationController
+};
