0cb25f44ddc2703404b68c8bd7f96138284c81b8
Sharad Bista <bishalbista737@gmail.com>
2026-05-01 23:03:37 +0545

    feat(backend): add index.js middleware

---
+++ backend/middleware/index.js
@@ -0,0 +1,7 @@
+const auth = require('./auth');
+const admin = require('./admin');
+const upload = require('./upload');
+const validation = require('./validation');
+const errorHandler = require('./errorHandler');
+
+module.exports = { auth, admin, upload, validation, errorHandler };
