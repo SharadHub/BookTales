c6e7efb8552c64ba0c53bfc82fde812f8621e279
Sharad Bista <bishalbista737@gmail.com>
2026-05-01 23:03:37 +0545

    feat: add utility function asyncHandler.js

---
+++ backend/utils/asyncHandler.js
@@ -0,0 +1,6 @@
+// Async handler wrapper (eliminates need for try-catch in controllers)
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+module.exports = asyncHandler;
