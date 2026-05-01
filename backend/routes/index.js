681fd9b812a9072d7e1b29988c1ef8c008f56ffc
Sharad Bista <bishalbista737@gmail.com>
2026-05-01 23:03:37 +0545

    feat(backend): add index.js routes

---
+++ backend/routes/index.js
@@ -0,0 +1,7 @@
+const auth = require('./auth');
+const books = require('./books');
+const reviews = require('./reviews');
+const admin = require('./admin');
+const dashboard = require('./dashboard');
+
+module.exports = { auth, books, reviews, admin, dashboard };
