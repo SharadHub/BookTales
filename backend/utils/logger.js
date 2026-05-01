0c33a24f4549e25b340012e27093259dd7c61806
Sharad Bista <bishalbista737@gmail.com>
2026-05-01 23:03:37 +0545

    feat: add utility function logger.js

---
+++ backend/utils/logger.js
@@ -0,0 +1,71 @@
+const winston = require('winston');
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'white',
+};
+
+winston.addColors(colors);
+
+const level = () => {
+  const env = process.env.NODE_ENV || 'development';
+  const isDevelopment = env === 'development';
+  return isDevelopment ? 'debug' : 'warn';
+};
+
+const format = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+  winston.format.colorize({ all: true }),
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+  ),
+);
+
+const transports = [
+  new winston.transports.Console(),
+  new winston.transports.File({
+    filename: 'logs/error.log',
+    level: 'error',
+  }),
+  new winston.transports.File({ filename: 'logs/all.log' }),
+];
+
+const logger = winston.createLogger({
+  level: level(),
+  levels,
+  format,
+  transports,
+  exitOnError: false,
+});
+
+const httpLogger = (req, res, next) => {
+  const start = Date.now();
+  
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    const message = `${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`;
+    
+    if (res.statusCode >= 400) {
+      logger.warn(message);
+    } else {
+      logger.http(message);
+    }
+  });
+  
+  next();
+};
+
+module.exports = {
+  httpLogger
+};
